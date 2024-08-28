0313736da0a6c26fd0c67dcd1eb8438603fce4e8
Alan Meniuk Gleizer <110932667+agleizer@users.noreply.github.com>
2024-08-28 00:30:26 +0000

    ex6 contabilizado

---
+++ 02_exs_sequencias/ex6.c
@@ -0,0 +1,13 @@
+/*
+6) Dada uma sequência de números inteiros com n elementos, faça uma função na
+linguagem C para determinar o comprimento de um segmento crescente de
+tamanho máximo.
+Exemplos:
+Na sequência 5 10 3 2 4 7 9 8 5 o comprimento do segmento crescente
+máximo é 4 (2, 4, 7, 9).
+Na sequência 10 8 7 5 2 o comprimento do segmento crescente máximo é 1.
+Qual é complexidade da sua função ?
+*/
+
+// feito como parte da minilista 01
+
