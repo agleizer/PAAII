b9359d3c7196e6e7923cb01ad0fe9990e06d4cc7
Alan Meniuk Gleizer <110932667+agleizer@users.noreply.github.com>
2024-08-31 15:54:33 +0000

    mais comentarios para passo-a-passo

--- 03_exs_backtracking/ex0.c
+++ 03_exs_backtracking/ex0.c
@@ -10,11 +10,16 @@ void printArray(int array[], int size) {
 
 int mostraSeq(int v[], int i, int s[], int tam) {
     printf("chamada de mostraSeq com i = %d\n", i);
-    if (i == tam) printArray(s, tam);
+    if (i == tam) {
+        printf("---> print de retorno: ");
+        printArray(s, tam);
+    }
     else {
         for (int j = 0; j < tam; j++) {
-            printf("j = %d\n", j);
+            printf("no for loop. j = %d, i = %d\n", j, i);
             s[i] = v[j];
+            printf("status: ");
+            printArray(s, tam);
             mostraSeq(v, i+1, s, tam);
         }
     }
@@ -22,6 +27,6 @@ int mostraSeq(int v[], int i, int s[], int tam) {
 
 int main() {
     int v[] = {1,2,3};
-    int s[3];
+    int s[] = {99,99,99}; // 99 só p ter ctz que o codespaces estava limpando a memoria
     mostraSeq(v, 0, s, 3);
 }
